58d228adf3ef94e1e0c49549e605cf914ba3126c
Alamsyah H. <45491807+tenggara25@users.noreply.github.com>
2025-12-09 20:56:14 +0700

    Update config.py

--- config.py
+++ config.py
@@ -3,7 +3,7 @@
 # Ganti URL ini dengan URL Web App yang keluar
 # dari menu: Deploy -> Manage deployments -> Web app -> Web app URL
 
-APPS_SCRIPT_POST_URL = "https://script.google.com/macros/s/AKfycbzopb64lIlbvtjRdhr7ZZakAcmEti1Pt_Jk3eSrNUAZAALGzro1qs0hru685OJlyyklbQ/exec"
+APPS_SCRIPT_POST_URL = "YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE"
 
 # GET URL sama, hanya ditambah query ?mode=sheet
-APPS_SCRIPT_GET_URL = "https://script.google.com/macros/s/AKfycbzopb64lIlbvtjRdhr7ZZakAcmEti1Pt_Jk3eSrNUAZAALGzro1qs0hru685OJlyyklbQ/exec?mode=sheet"
+APPS_SCRIPT_GET_URL = "YOUR_GOOGLE_APPS_SCRIPT_WEB_APP_URL_HERE?mode=sheet"
